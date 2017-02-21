ddd308702d25645e7a7af6d20dfa4f771b9ca4ac
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-02-21 00:16:43 -0700

    Upgrade to v4 beta API

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Match, BrowserRouter, Link, Miss, Redirect } from 'react-router'
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
 import Home from './Home'
@@ -8,9 +8,9 @@ import Dashboard from './protected/Dashboard'
 import { logout } from '../helpers/auth'
 import { firebaseAuth } from '../config/constants'
 
-function MatchWhenAuthed ({component: Component, authed, ...rest}) {
+function PrivateRoute ({component: Component, authed, ...rest}) {
   return (
-    <Match
+    <Route
       {...rest}
       render={(props) => authed === true
         ? <Component {...props} />
@@ -19,9 +19,9 @@ function MatchWhenAuthed ({component: Component, authed, ...rest}) {
   )
 }
 
-function MatchWhenUnauthed ({component: Component, authed, ...rest}) {
+function PublicRoute ({component: Component, authed, ...rest}) {
   return (
-    <Match
+    <Route
       {...rest}
       render={(props) => authed === false
         ? <Component {...props} />
@@ -55,49 +55,48 @@ export default class App extends Component {
   render() {
     return this.state.loading === true ? <h1>Loading</h1> : (
       <BrowserRouter>
-        {({router}) => (
-          <div>
-            <nav className="navbar navbar-default navbar-static-top">
-              <div className="container">
-                <div className="navbar-header">
-                  <Link to="/" className="navbar-brand">React Router + Firebase Auth</Link>
-                </div>
-                <ul className="nav navbar-nav pull-right">
-                  <li>
-                    <Link to="/" className="navbar-brand">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
-                  </li>
-                  <li>
-                    {this.state.authed
-                      ? <button
-                          style={{border: 'none', background: 'transparent'}}
-                          onClick={() => {
-                            logout()
-                            this.setState({authed: false})
-                            router.transitionTo('/')
-                          }}
-                          className="navbar-brand">Logout</button>
-                      : <span>
-                          <Link to="/login" className="navbar-brand">Login</Link>
-                          <Link to="/register" className="navbar-brand">Register</Link>
-                        </span>}
-                  </li>
-                </ul>
-              </div>
-            </nav>
+        <div>
+          <nav className="navbar navbar-default navbar-static-top">
             <div className="container">
-              <div className="row">
-                <Match pattern='/' exactly component={Home} />
-                <MatchWhenUnauthed authed={this.state.authed} pattern='/login' component={Login} />
-                <MatchWhenUnauthed authed={this.state.authed} pattern='/register' component={Register} />
-                <MatchWhenAuthed authed={this.state.authed} pattern='/dashboard' component={Dashboard} />
-                <Miss render={() => <h3>No Match</h3>} />
+              <div className="navbar-header">
+                <Link to="/" className="navbar-brand">React Router + Firebase Auth</Link>
               </div>
+              <ul className="nav navbar-nav pull-right">
+                <li>
+                  <Link to="/" className="navbar-brand">Home</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
+                </li>
+                <li>
+                  {this.state.authed
+                    ? <button
+                        style={{border: 'none', background: 'transparent'}}
+                        onClick={() => {
+                          logout()
+                          this.setState({authed: false})
+                        }}
+                        className="navbar-brand">Logout</button>
+                    : <span>
+                        <Link to="/login" className="navbar-brand">Login</Link>
+                        <Link to="/register" className="navbar-brand">Register</Link>
+                      </span>}
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+                <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+                <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
             </div>
           </div>
-        )}
+        </div>
       </BrowserRouter>
     );
   }
