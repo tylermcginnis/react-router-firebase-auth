ddebbcb061b0a078f00493ea947787847f0570b0
Jon Samp <jon@codecademy.com>
2017-03-12 15:20:53 -0400

    Adds login/register errors, password reset

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import { auth } from '../helpers/auth'
 
+function setErrorMsg(error) {
+  return {
+    registerError: error.message
+  }
+}
+
 export default class Register extends Component {
+  state = { registerError: null }
   handleSubmit = (e) => {
     e.preventDefault()
     auth(this.email.value, this.pw.value)
+      .catch(e => this.setState(setErrorMsg(e)))
   }
   render () {
     return (
@@ -19,6 +27,14 @@ export default class Register extends Component {
             <label>Password</label>
             <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
           </div>
+          {
+            this.state.registerError &&
+            <div className="alert alert-danger" role="alert">
+              <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+              <span className="sr-only">Error:</span>
+              &nbsp;{this.state.registerError}
+            </div>
+          }
           <button type="submit" className="btn btn-primary">Register</button>
         </form>
       </div>
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -3,7 +3,6 @@ import { ref, firebaseAuth } from '../config/constants'
 export function auth (email, pw) {
   return firebaseAuth().createUserWithEmailAndPassword(email, pw)
     .then(saveUser)
-    .catch((error) => console.log('Oops', error))
 }
 
 export function logout () {
@@ -12,6 +11,10 @@ export function logout () {
 
 export function login (email, pw) {
   return firebaseAuth().signInWithEmailAndPassword(email, pw)
+}
+
+export function resetPassword (email) {
+  return firebaseAuth().sendPasswordResetEmail(email)
 }
 
 export function saveUser (user) {
