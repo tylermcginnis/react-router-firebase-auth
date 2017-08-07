5c7f961d96e0d1812b558b85e1d0bd1f23090284
Ron-Reagans_Ghost <alexander.singleton40@gmail.com>
2017-08-07 16:08:45 -0400

    Editing registration page and profile page for required ProvaSport info

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
+//import { ref } from '../src/config/constants'
+//import { auth } from '../helpers/auth'
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group'
 
 export default class Dashboard extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      newProfile: {
+        sports : null,
+        age: null
+      }
+    }
+  }
+
+  sportsChanged(newSports) {
+    this.setState({
+      sports: newSports
+    });
+  }
+
+  onChange(value) {
+    this.setState({
+      age: value
+    });
+    }
+
+  handleSubmit(e){
+    // ref.child(`users/${this.props.user.uid}/info`)
+    //   .set({
+    //     sports: this.sports,
+    //     age: this.age
+    //   })
+  }
+
   render () {
     return (
       <div>
       <h1> Welcome!</h1>
       <h2> Please fill in your profile so we can instantly
         match you with games near you! </h2>
+
+              <CheckboxGroup
+                 name="favoriteSports"
+                 value={this.state.sports}
+                 onChange={this.sportsChanged.bind(this)}>
+                     <label><Checkbox value="tennis"/> Tennis</label>
+                     <label><Checkbox value="badminton"/> Badminton</label>
+                     <label><Checkbox value="basketball"/> Basketball</label>
+                     <label><Checkbox value="soccer"/> Soccer</label>
+                     <label><Checkbox value="softball"/> Softball</label>
+                   </CheckboxGroup>
+                   <input type="submit" value="Update Profile" />
       </div>
     )
   }
