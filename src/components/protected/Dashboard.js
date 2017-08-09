1a7048737d8248dac85b16b44add66be2d39c44f
Ron-Reagans_Ghost <alexander.singleton40@gmail.com>
2017-08-09 01:44:55 -0400

    Close to finished on createMatch implem.

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 //import { ref } from '../src/config/constants'
 //import { auth } from '../helpers/auth'
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 import {Checkbox, CheckboxGroup} from 'react-checkbox-group'
+import createMatch from './createMatch'
+//import DatePicker from 'react-bootstrap-date-picker'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import dateFormat from 'dateformat'
+import { ref, firebaseAuth } from '../config/constants'
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class Dashboard extends Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       newProfile: {
         sports : null,
-        age: null
+        age: null,
+        startDate: moment()
       }
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   sportsChanged(newSports) {
@@ -27,16 +38,31 @@ export default class Dashboard extends Component {
     });
     }
 
-  handleSubmit(e){
-    // ref.child(`users/${this.props.user.uid}/info`)
-    //   .set({
-    //     sports: this.sports,
-    //     age: this.age
-    //   })
+  handleChange(date){
+     this.setState({startDate: date
+     });
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  firebaseAuth().onAuthStateChanged(function(user) {
+if (user) {
+// User is signed in.
+ref.child(`users/${user.uid}/matches`)
+.push({
+  Sport: this.sport,
+  gameDate: this.startDate,
+  Skill: this.skill
+})
+} else {
+// No user is signed in.
+}
+});
+}
 
   render () {
     return (
+      <div>
       <div>
       <h1> Welcome!</h1>
       <h2> Please fill in your profile so we can instantly
@@ -53,6 +79,53 @@ export default class Dashboard extends Component {
                      <label><Checkbox value="softball"/> Softball</label>
                    </CheckboxGroup>
                    <input type="submit" value="Update Profile" />
+                   </div>
+
+      <h2> Create a Match: </h2>
+      <Router>
+      <div>
+     <li><NavLink to="/protected/createMatch">Create Match</NavLink></li>
+           <Route path="/protected/createMatch" component={createMatch}/>
+           </div>
+           </Router>
+           <form onSubmit={this.handleSubmit}>
+           <label>Sport</label>
+           <div className="form-group">
+           <select id="sport" ref={(sport) => this.sport = sport}>
+           <option disabled value>Sport</option>
+           <option value="1">Tennis</option>
+           <option value="2">Badminton</option>
+           <option value="3">Basketball</option>
+           <option value="4">Soccer</option>
+           <option value="5">Softball</option>
+           </select>
+
+
+           </div>
+           <label>Date</label>
+           <div className="date">
+           <DatePicker
+           selected={this.state.startDate}
+           onChange={this.handleChange}
+           minDate={moment()}
+           maxDate={moment().add(65, "days")}
+           placeholderText="Choose a Day" />
+           </div>
+
+
+           <label>Skill Level</label>
+           <div className="form-group">
+           <select id="skill" ref={(skill) => this.skill = skill}>
+           <option disabled value>Skill</option>
+           <option value="1">Beginner</option>
+           <option value="2">Intermediate</option>
+           <option value="3">Advanced</option>
+           </select>
+
+           <button type="submit" className="btn btn-primary">Create</button>
+
+           </div>
+         </form>
       </div>
     )
   }
---
+++ src/components/protected/createMatch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class createMatch extends Component {
+  render () {
+    return (
+      <div>
+        What match would you like to create?
+      </div>
+    )
+  }
+}
